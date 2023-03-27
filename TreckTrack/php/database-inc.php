9e8553ec4b0ac282d8b24f89f381313bdf05e474
Batu Durmazel <batudurmazel@gmail.com>
2023-03-27 22:03:29 +0300

    added csv creation

---
+++ TreckTrack/php/database-inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$serverName = "localhost";
+$dbUserame = "root";
+$dbPassword = "";
+$dbName = "TrekTrack";
+
+$conn = mysqli_connect($serverName, $dbUserame, $dbPassword, $dbName);
+
+if (!conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
